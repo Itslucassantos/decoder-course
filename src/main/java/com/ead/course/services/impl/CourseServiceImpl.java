fa8f9ff5d2f7142981d067f4d494074a7bb0c69f
Itslucassantos <ls6865316@gmail.com>
2025-01-20 18:59:11 -0300

    Deleção de Relacionamento Cascade.ALL ou Método de Deleção Customizado

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,9 +1,17 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService {
@@ -11,4 +19,26 @@ public class CourseServiceImpl implements CourseService {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private ModuleRepository moduleRepository;
+
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Transactional
+    @Override
+    public void delete(CourseModel courseModel) {
+        List<ModuleModel> moduleModelList = this.moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+        if (!moduleModelList.isEmpty()) {
+            for (ModuleModel moduleModel : moduleModelList) {
+                List<LessonModel> lessonModelList = this.lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
+                if (!lessonModelList.isEmpty()) {
+                    this.lessonRepository.deleteAll(lessonModelList);
+                }
+            }
+            this.moduleRepository.deleteAll(moduleModelList);
+        }
+
+        this.courseRepository.delete(courseModel);
+    }
 }
